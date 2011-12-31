c64e532402b9dfba9d84e00030d284f9227cde48
rick <rick_barkley@me.com>
2011-12-31 16:59:35 -0600

    game update

--- my_game.rb
+++ my_game.rb
@@ -2,43 +2,65 @@ $: << File.expand_path(File.dirname(__FILE__))
 require 'rubygems'
 require 'gosu'
 require 'player'
+require 'ball'
 
 
 class MyGame < Gosu::Window
   def initialize
     super(600, 400, false)
-    @player = Player.new(self)
+    @player1 = Player.new(self)
+    @balls = 3.times.map {Ball.new(self)}
+    @running = true
   end
 
   def update
-  	if button_down? Gosu::Button::KbLeft
-  		@player.move_left
-  	end
-  	if button_down? Gosu::Button::KbRight
-  		@player.move_right
-  	
-  	end
-  	
-  	if button_down? Gosu::Button::KbDown
-  		@player.move_down
-  		end
-  		
-  	if button_down? Gosu::Button::KbUp
-  		@player.move_up
-  		
-  	end
-  	end
-  	 	 
-  	
-    def draw
-    	@player.draw
-	end
-	
- 
-  
+    if @running
+      if button_down? Gosu::Button::KbLeft
+        @player1.move_left
+      end
+
+      if button_down? Gosu::Button::KbRight
+        @player1.move_right
+      end
+
+      if button_down? Gosu::Button::KbUp
+        @player1.move_up
+      end
+
+      if button_down? Gosu::Button::KbDown
+        @player1.move_down
+      end
+
+      @balls.each {|ball| ball.update}
+
+      if @player1.hit_by? @balls
+        stop_game!
+      end
+
+    else
+      if button_down? Gosu::Button::KbEscape
+        restart_game
+      end
+    end
+  end
+
+  def draw
+    @player1.draw
+    @balls.each {|ball| ball.draw}
+  end
+
+  def stop_game!
+    @running = false
+  end
+
+  def restart_game
+    @running = true
+    @balls.each {|ball| ball.reset!}
+  end
+
 end
-window = MyGame.new
-window.show
+game = MyGame.new
+game.show
 
 
 	
